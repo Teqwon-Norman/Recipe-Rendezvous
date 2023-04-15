08622cd3d37cedcd375e74e5b5efa33d11edaaf9
Teqwon Norman <teqwon.norman@outlook.com>
2023-04-15 07:32:18 -0400

    Squashed commit of the following:
    commit cf75b471b1b748b989ca1f6d4dea933bae92a275
    Author: Teqwon Norman <teqwon.norman@outlook.com>
    Date:   Sat Apr 15 07:31:56 2023 -0400
    
        finished setting up user authentication

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,23 @@
-from flask import Flask, request
-from flask_restx import Api, Resource, fields
-from config import DevConfig
-from models import Recipe
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
+from flask_restx import Api, Resource, fields
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from config import DevConfig
+from models import Recipe, User
 from exts import db
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
-db.init_app(app)
-migrate = Migrate(app, db)
+
 api = Api(app, doc='/docs')
+
+db.init_app(app)
+
+JWTManager(app)
+migrate = Migrate(app, db)
+
 
 # model (serializer)
 recipe_model = api.model(
@@ -21,11 +29,77 @@ recipe_model = api.model(
     }
 )
 
+# signup schema
+signup_model = api.model(
+    'SignUp',
+    {
+        'username': fields.String(),
+        'email': fields.String(),
+        'password': fields.String()
+    }
+)
+
+# login schema
+login_model = api.model(
+    'Login',
+    {
+        'username': fields.String(),
+        'password': fields.String()
+    }
+)
 
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {'message': 'Hello World'}
+
+@api.route('/signup')
+@api.expect(signup_model)
+class SignUp(Resource):
+    @api.marshal_list_with(signup_model)
+    def get(self):
+        """ Get all users """
+        data = User.query.all()
+        return data
+
+    def post(self):
+        """ Create a new user """
+        data = request.get_json()
+
+        # check if username exists in database already
+        username = data.get('username')
+        db_user = User.query.filter_by(username=username).first()
+        if db_user: return jsonify({'message' : f'User with username {username} already exists!!'})
+
+        new_user = User(
+            username = data.get('username'),
+            email = data.get('email'),
+            password = generate_password_hash(data.get('password'), 'sha256')
+        )
+
+        new_user.save()
+        return jsonify({'message' : 'User created successfully!'})
+
+@api.route('/login')
+class Login(Resource):
+    @api.expect(login_model)
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        db_user = User.query.filter_by(username=username).first()
+        if db_user and check_password_hash(db_user.password, password):
+            access_token = create_access_token(identity=db_user.username)
+            refresh_token = create_refresh_token(identity=db_user.username)
+            return jsonify(
+                {
+                    'access token' : access_token,
+                    'refresh token' : refresh_token
+                }
+            )
+        return jsonify({'message' : 'User does not exist!'}) 
 
 
 @api.route('/recipes')
@@ -37,6 +111,8 @@ class RecipesResource(Resource):
         return recipes
 
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
+    @jwt_required()
     def post(self):
         """Create a new recipe """
         data = request.get_json()
@@ -59,7 +135,8 @@ class RecipeResource(Resource):
         return recipe
 
     @api.marshal_with(recipe_model)
-    def put(self, id):
+    @jwt_required()
+    def put(self, id): 
         """Update a recipe by id"""
         recipe_to_update = Recipe.query.get_or_404(id)
         data = request.get_json()
@@ -67,6 +144,7 @@ class RecipeResource(Resource):
         return recipe_to_update
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def delete(self, id):
         """Delete a recipe by id"""
         recipe_to_delete = Recipe.query.get_or_404(id)
